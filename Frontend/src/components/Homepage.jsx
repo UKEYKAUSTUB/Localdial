d6031e616aa2c582eb0b57ccaad4961f1d6ff653
Kaustubh Ukey <142865070+UKEYKAUSTUB@users.noreply.github.com>
2025-01-26 19:56:35 +0530

    Create Homepage.jsx

---
+++ Frontend/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+const Homepage =() =>
+{
+    return(
+        <div className="py-10"
+        style={{
+          background: 'linear-gradient(to bottom, rgba(255, 255, 255, 1),rgba(72, 187, 97, 0.05))',}}>
+            <h1 className="text-6xl font-extrabold text-black leading-tight">This is my homepage</h1>
+        </div>
+    );
+}
+export default Homepage
