8734249f860db7337f8ff279786e8e10c67dcc4e
Kaustubh Ukey <142865070+UKEYKAUSTUB@users.noreply.github.com>
2025-01-26 12:10:29 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:5000/api/v1/auth/login", {
+      const response = await axios.post("/api/v1/auth/login", {
         email,
         password,
       });
