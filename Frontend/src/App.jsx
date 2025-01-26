2d68f8a65c717715fd83cfe488604dc0bcca8c55
Kaustubh Ukey <142865070+UKEYKAUSTUB@users.noreply.github.com>
2025-01-26 19:55:27 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,23 +5,34 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Footer from './components/Footer';
+import Homepage from './components/Homepage'
 
 const AppContent = () => {
   const location = useLocation();
 
   // Define routes where Navbar should not be displayed
   const hideNavbarRoutes = ["/signup","/login"];
+  const hideFooterRoutes = ["/homepage"];
+  console.log("Current Pathname:", location.pathname);
+  console.log("Should Hide Footer:", hideFooterRoutes.includes(location.pathname));
 
   return (
     <>
       {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+     
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/homepage' element={<Homepage/>}/>
+        
         
       </Routes>
-      <Footer/>
+      
+
+      {!hideFooterRoutes.includes(location.pathname) && <Footer/>}
+      
+
     </>
   );
 };
