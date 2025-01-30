eab9f2b2c4f4ad5f7c8fdb290648cf2bd201ed57
Kaustubh Ukey <142865070+UKEYKAUSTUB@users.noreply.github.com>
2025-01-30 17:39:36 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/v1/auth/post", {
+      const response = await axios.post("https://localdial-lv6n.onrender.com", {
         name,
         email,
         password,
